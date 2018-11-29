7bd20bb0e81a9b2572e29a0d17f63e2bfd38b66c
Jerinho <jerinho@users.noreply.github.com>
2018-11-29 16:26:05 +0800

    Update App.js

--- app/App.js
+++ app/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import ReactModule from 'react-native-mylibrary';
+import {ReactModule} from 'react-native-mylibrary';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
